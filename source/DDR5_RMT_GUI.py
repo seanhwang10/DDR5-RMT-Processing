ac433416840af1a338f1f4e36785d0520a537ec5
Jean Mattekatt -X (jemattek - PTEC SOLUTIONS INC at Cisco) <jemattek@cisco.com>
2023-07-27 14:19:17 -0700

    reads multiple folders

--- source/DDR5_RMT_GUI.py
+++ source/DDR5_RMT_GUI.py
@@ -11,8 +11,9 @@ def jean_analysis():
     numData = int(num_data_var.get())
     folders = []
     for i in range(0, numData):
-        folders.append(folder_path_var.get())
-        # folders = folder_path_var.get()
+        # folders.append(get_folders(i + 1))
+        folders.append(browse_folder())
+    # askBooleanInput(numData + 1)
     bootstrap = "Y" if bootstrap_var.get() else "N"
     includeLine = "Y" if include_line_var.get() else "N"
 
@@ -26,7 +27,6 @@ def jean_analysis():
 
 
     # ---- Your code should begin here ----------------------------------------------------------- #
-
     vendorNames = []
     for i in range(0, numData):
         name = folders[i][folders[i].rfind("/")+1:folders[i].rfind("_")]
@@ -42,8 +42,17 @@ def jean_analysis():
 # ---GUI Code Below ------------------------------------------------------------------------------- #
 
 def browse_folder():
-    folder_path = filedialog.askdirectory()
-    folder_path_var.set(folder_path)
+    # folder_path = filedialog.askdirectory()
+    # folder_path_var.set(folder_path)
+    return filedialog.askdirectory()
+
+# def get_folders(r):
+#     folder_label = tk.Label(root, text="Path to folder:")                                           # folder
+#     folder_label.grid(row=r, column=0, padx=10, pady=5)
+#     folder_entry = tk.Entry(root, textvariable=folder_path_var)
+#     folder_entry.grid(row=r, column=1, padx=10, pady=5)
+#     browse_button = tk.Button(root, text="Browse", command=browse_folder)
+#     browse_button.grid(row=r, column=2, padx=10, pady=5)
 
 root = tk.Tk()
 root.title("DDR5 RMT GUI (Prototype)")
@@ -59,28 +68,30 @@ num_label.grid(row=0, column=0, padx=10, pady=5)
 num_data_combobox = ttk.Combobox(root, textvariable=num_data_var, values=[1, 2, 3, 4, 5])
 num_data_combobox.grid(row=0, column=1, padx=10, pady=5)
 
-folder_label = tk.Label(root, text="Path to folder:")                                           # folder
-folder_label.grid(row=1, column=0, padx=10, pady=5)
-folder_entry = tk.Entry(root, textvariable=folder_path_var)
-folder_entry.grid(row=1, column=1, padx=10, pady=5)
-browse_button = tk.Button(root, text="Browse", command=browse_folder)
-browse_button.grid(row=1, column=2, padx=10, pady=5)
+# folder_label = tk.Label(root, text="Path to folder:")                                           # folder
+# folder_label.grid(row=1, column=0, padx=10, pady=5)
+# folder_entry = tk.Entry(root, textvariable=folder_path_var)
+# folder_entry.grid(row=1, column=1, padx=10, pady=5)
+# browse_button = tk.Button(root, text="Browse", command=browse_folder)
+# browse_button.grid(row=1, column=2, padx=10, pady=5)
 
 bootstrap_label = tk.Label(root, text="Bootstrap?")                                             # bootstrap
-bootstrap_label.grid(row=2, column=0, padx=10, pady=5)
+bootstrap_label.grid(row=1, column=0, padx=10, pady=5)
 bootstrap_yes_radio = tk.Radiobutton(root, text="Yes", variable=bootstrap_var, value=True)
-bootstrap_yes_radio.grid(row=2, column=1, padx=10, pady=5)
+bootstrap_yes_radio.grid(row=1, column=1, padx=10, pady=5)
 bootstrap_no_radio = tk.Radiobutton(root, text="No", variable=bootstrap_var, value=False)
-bootstrap_no_radio.grid(row=2, column=2, padx=10, pady=5)
+bootstrap_no_radio.grid(row=1, column=2, padx=10, pady=5)
 
 include_line_label = tk.Label(root, text="VM Line?")                                            # includeLine
-include_line_label.grid(row=3, column=0, padx=10, pady=5)
+include_line_label.grid(row=2, column=0, padx=10, pady=5)
 include_line_yes_radio = tk.Radiobutton(root, text="Yes", variable=include_line_var, value=True)
-include_line_yes_radio.grid(row=3, column=1, padx=10, pady=5)
+include_line_yes_radio.grid(row=2, column=1, padx=10, pady=5)
 include_line_no_radio = tk.Radiobutton(root, text="No", variable=include_line_var, value=False)
-include_line_no_radio.grid(row=3, column=2, padx=10, pady=5)
+include_line_no_radio.grid(row=2, column=2, padx=10, pady=5)
 
 run_button = tk.Button(root, text="Run", command=jean_analysis, width=20)
-run_button.grid(row=4, column=0, columnspan=3, padx=10, pady=20)
+run_button.grid(row=3, column=0, columnspan=3, padx=10, pady=20)
+
+
 
 root.mainloop()
